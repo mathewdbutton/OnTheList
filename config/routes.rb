3a81f446f240dee6020119945bef693834b20c36
Mathew Button <mat@pushbutton.xyz>
2020-06-19 22:30:08 +1000

    Add dashboard "home page"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "lists#index"
 
   resources :lists, only: [:show, :index]
 
