890182be1af84e89e537ca7f9331586b6b2687df
Mathew Button <mat@pushbutton.xyz>
2020-06-21 13:44:55 +1000

    Allow configurable delete button for lists

--- app/components/list_record_component.rb
+++ app/components/list_record_component.rb
@@ -1,6 +1,7 @@
 class ListRecordComponent < ViewComponent::Base
-  attr_reader :list
-  def initialize(list_record:)
+  attr_reader :list, :allow_delete
+  def initialize(list_record:, allow_delete: false)
     @list = list_record
+    @allow_delete = allow_delete
   end
 end
