9c0b03fede181f714b6fe3269bee3bd940c651ae
Mathew Button <mat@pushbutton.xyz>
2020-06-21 13:21:56 +1000

    Show 5 recent updated items on dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def dashboard
     @lists = List.limit(5).order(updated_at: :desc).all
+    @items = Item.limit(5).order(updated_at: :desc).all
   end
 end
