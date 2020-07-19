5d1321ae49ed23ac06cbde8c2b7c3f35f11e015f
Mathew Button <mat@pushbutton.xyz>
2020-07-19 20:20:54 +1000

    Add a helper to set caching option

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def turbolink_caching_directive
+    return "" unless content_for?(:turbolink_caching_directive)
+    "<meta name=\"turbolinks-cache-control\" content=\"#{content_for(:turbolink_caching_directive)}\">"
+  end
 end
