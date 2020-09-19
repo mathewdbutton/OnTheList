018596f74fc84adcdf1d9aa34d9741483122c8e8
Mathew Button <mat@pushbutton.xyz>
2020-09-19 18:03:19 +1000

    Delete method_recipe_item if recipe item deleted

--- app/models/recipe_item.rb
+++ app/models/recipe_item.rb
@@ -2,5 +2,7 @@ class RecipeItem < ApplicationRecord
   belongs_to :recipe
   belongs_to :item
 
+  has_many :method_recipe_items, dependent: :delete_all
+
   accepts_nested_attributes_for :item
 end
