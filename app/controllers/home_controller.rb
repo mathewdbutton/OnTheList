cbe42bad1ec2a377f44b55e3e9a4ec5ef4f6b6f9
Mathew Button <mat@pushbutton.xyz>
2020-09-30 21:49:24 +1000

    Remove feature flag for recipes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def dashboard
-    @show_recipes = false
-    if request.headers["show-recipe"] == "yep"
-      @show_recipes = true
-    end
     @lists = List.limit(5).order(updated_at: :desc).all
     @items = Item.limit(5).order(updated_at: :desc).all
   end
