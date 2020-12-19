2625ff6597375700cf1eb28d36bb5fee4ad1e0f2
Mathew Button <mat@pushbutton.xyz>
2020-12-20 10:42:05 +1100

    Add theming to dashboard home page

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,10 +1,22 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        primary: colors.purple[600],
+        secondary: "#FFC42A",
+        background: colors.gray[100]
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['active'],
+      textColor: ['active']
+    },
   },
   plugins: [],
 };
